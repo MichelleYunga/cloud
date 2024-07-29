7aad064d5f15fc7070b609cde3a0bf427970b790
MichelleYunga <111171604+MichelleYunga@users.noreply.github.com>
2024-07-29 02:26:42 -0500

    Delete src directory

--- src/main/java/com/example/clound/REPOSITORY/PlaylistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.clound.REPOSITORY;
-
-import com.example.clound.MODELO.Playlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-    Playlist findByName(String name);
-}
--- src/main/java/com/example/clound/REPOSITORY/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.clound.REPOSITORY;
-
-import com.example.clound.MODELO.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-}
